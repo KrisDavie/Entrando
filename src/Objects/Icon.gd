1dc7dd0cf37a02ed6a0e0df39b7126d7983e2bc6
KrisDavie <kristofer.davie@gmail.com>
2025-03-16 16:17:23 +0100

    Mark off items on coop partners when done manually

--- src/Objects/Icon.gd
+++ src/Objects/Icon.gd
@@ -33,8 +33,7 @@ func _input_event(_viewport: Object, event: InputEvent, _shape_idx: int) -> void
         and event.is_pressed():
             hide()
             Util.add_hidden(self)
-            if !is_item:
-                Events.emit_signal("coop_send_update", {
-                    "event": "remove_location",
-                    "node_path": get_path()
-                })
+            Events.emit_signal("coop_send_update", {
+                "event": "remove_location",
+                "node_path": get_path()
+            })
